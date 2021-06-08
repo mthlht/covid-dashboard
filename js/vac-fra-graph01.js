7f4c455fb438796648a34540cc075be9c4a93eb8
Mth Lht <mathieu.lehot@protonmail.com>
2021-06-08 06:10:55 +0200

    'ajout vac-fra-graph01'

--- js/vac-fra-graph01.js
+++ js/vac-fra-graph01.js
@@ -1,4 +1,4 @@
-d3.csv("data/spf_fra_data.csv").then(showData);
+d3.csv("../data/spf_fra_vacc.csv").then(showData);
 
 function showData(data) {
 
@@ -24,18 +24,18 @@ function showData(data) {
     const tempData = data.map(d => {
 
         let newData = {
-            "date": new Date(d.date), // ATTENTION À TRANSPOSER EN FORMAT DATE
-            "total_hosp": +d.total_hosp, // ATTENTION STRING A TRANSPOSER EN FLOAT
-            "part_rea": +d.total_rea, // ATTENTION STRING A TRANSPOSER EN FLOAT
-            "part_hosp": (+d.total_hosp)-(+d.total_rea) // ATTENTION STRING A TRANSPOSER EN FLOAT
+            "date": new Date(d.jour), // ATTENTION À TRANSPOSER EN FORMAT DATE
+            "cum_dose1": +d.n_cum_dose1, // ATTENTION STRING A TRANSPOSER EN FLOAT
+            "cum_dose2": +d.n_cum_complet, // ATTENTION STRING A TRANSPOSER EN FLOAT
+            "cum_total": (+d.n_cum_dose1) + (+d.n_cum_complet) // ATTENTION STRING A TRANSPOSER EN FLOAT
         }
 
         return newData
 
     });
 
-    // Filtre les données uniquement à partir du 1er septembre
-    const tidyData = tempData.filter(d => d.date >= new Date("2020-09-01"));
+    // Filtre les données uniquement à partir du 1er janvier
+    const tidyData = tempData.filter(d => d.date >= new Date("2021-01-01"))
 
     //---------------------------------------------------------------------------------------
 
@@ -48,8 +48,12 @@ function showData(data) {
     const marginV = 20;
     const leg = 40;
 
+    // variables d'ajustement du graphique pour les noms des régions
+    const marginHratio = marginH * 0.2; // uniquement utilisée pour le widthRatio
+    const widthRatio = width - marginHratio; // uniquement utilisée pour l'échelle scaleX
+
     // création du canevas pour le Graphique
-    const svg = d3.select('#fra-nat-graph04 .graph')
+    const svg = d3.select('#vac-fra-graph01 .graph')
         .append("svg")
         .attr("viewBox", [0, 0, width + marginH * 2, height + leg + marginV * 2])
         .attr("preserveAspectRatio", "xMinYMid");
@@ -77,23 +81,23 @@ function showData(data) {
     let paddingTitles = svgSizeInNav / totalDims * marginH;
 
     // Écriture du titre
-    const title = d3.select('#fra-nat-graph04 .graph-title')
-        .html('Evolution du nombre de patients Covid hospitalisés')
+    const title = d3.select('#vac-fra-graph01 .graph-title')
+        .html('Evolution du nombre de vaccinations contre le Covid-19')
         .style('padding-right', paddingTitles + "px")
         .style('padding-left', paddingTitles + "px");
 
     // Écriture du sous-titre
-    const subtitle = d3.select('#fra-nat-graph04 .graph-subtitle')
-        .html('depuis septembre 2020')
+    const subtitle = d3.select('#vac-fra-graph01 .graph-subtitle')
+        .html('depuis janvier 2021')
         .style('padding-right', paddingTitles + "px")
         .style('padding-left', paddingTitles + "px");
 
     // Écriture
-    const caption = d3.select('#fra-nat-graph04 .graph-caption')
+    const caption = d3.select('#vac-fra-graph01 .graph-caption')
         .html("Source. <a href='https://www.data.gouv.fr/fr/organizations/sante-publique-france/' target='_blank'>Santé publique France</a>")
         .style('padding-right', paddingTitles + "px")
         .style('padding-left', paddingTitles + "px");
-    
+
     // Adaptation du padding à chaque resize de la fenêtre du navigateur
     d3.select(window).on("resize", () => {
 
@@ -121,14 +125,14 @@ function showData(data) {
 
     // échelle linéaire pour l'axe des Y
     const scaleY = d3.scaleLinear()
-        .domain([0, d3.max(tidyData, d => d.total_hosp)])
+        .domain([0, d3.max(tidyData, d => d.cum_dose1)])
         .range([height, 0]);
 
     // échelee temporelle pour l'axe des X
     const scaleT = d3.scaleTime()
         .domain([d3.min(tidyData, d => d.date),
         d3.max(tidyData, d => d.date)])
-        .range([0, width]);
+        .range([0, widthRatio]);
 
     //---------------------------------------------------------------------------------------
 
@@ -141,14 +145,14 @@ function showData(data) {
             .ticks(4)
             .tickFormat(d3.timeFormat("%b %Y")))
         .selectAll('text')
-        .style("fill", "grey") // couleur du texte
+        .style("fill", "grey"); // couleur du texte
 
     // Axe des Y
     const yAxis = g => g
         .attr("transform", `translate(0, 0)`)
         .call(d3.axisLeft(scaleY)
-            .ticks(8) // Nombre de ticks
-            .tickFormat(d => d.toLocaleString('fr-FR'))) // formatage grands nombre avec espace entre milliers
+            .ticks(5) // Nombre de ticks
+            .tickFormat(d => d/1000000 + 'M')) // formatage grands nombre avec espace entre milliers
         .call(g => g.select(".domain").remove()) // supprime la ligne de l'axe
         .selectAll('text')
         .style("fill", "grey") // couleur du texte
@@ -172,53 +176,73 @@ function showData(data) {
 
     //---------------------------------------------------------------------------------------
 
-    // Création du Stack Area Chart
-    
-    // générateur de l'aire des hospitalisations
-    const areaHosp = d3.area()
+    // Création du stack area chart
+
+    // générateur de l'aire des premières injections
+    const areaDose1 = d3.area()
         .curve(d3.curveLinear)
         .x(d => scaleT(d.date))
-        .y0(d => scaleY(d.part_rea))
-        .y1(d => scaleY(d.part_hosp));
-    
-    // générateur de l'aire des réanimations
-    const areaRea = d3.area()
+        .y0(d => scaleY(d.cum_dose2))
+        .y1(d => scaleY(d.cum_dose1));
+
+    // générateur de l'aire des vaccinations complètes
+    const areaDose2 = d3.area()
         .curve(d3.curveLinear)
         .x(d => scaleT(d.date))
         .y0(scaleY(0))
-        .y1(d => scaleY(d.part_rea));
-    
-    // projection de l'aire des hospitalisations
+        .y1(d => scaleY(d.cum_dose2));
+
+    // projection de l'aire des premières injections
     svgPlot.append("path")
         .datum(tidyData)
         .attr("fill", "#0072B2")
-        .attr("d", areaHosp)
-        .attr("opacity", 0.6)
-    
-    // projection de l'aire des réanimations
+        .attr("d", areaDose1)
+        .attr("opacity", 0.6);
+
+    // projection de l'aire des vaccinations complètes
     svgPlot.append("path")
         .datum(tidyData)
         .attr("fill", "#D55E00")
-        .attr("d", areaRea)
+        .attr("d", areaDose2);
 
     //---------------------------------------------------------------------------------------
 
-    // Affichage des dernières valeurs
+    // Cercles des dernières valeurs
 
     const maxDate = d3.max(tidyData, d => d.date);
 
     const maxVal = tidyData.filter(d => d.date == maxDate);
 
+    svgPlot.append("circle")
+        .attr("cx", scaleT(maxVal[0].date))
+        .attr("cy", scaleY(maxVal[0].cum_dose2))
+        .attr("r", 4)
+        .attr("stroke", "#ffffff")
+        .attr("stroke-width", 0.8)
+        .attr("fill", "#D55E00");
+
+    svgPlot.append("circle")
+        .attr("cx", scaleT(maxVal[0].date))
+        .attr("cy", scaleY(maxVal[0].cum_dose1))
+        .attr("r", 4)
+        .attr("stroke", "#ffffff")
+        .attr("stroke-width", 0.8)
+        .attr("fill", "#0072B2");
+
+    //---------------------------------------------------------------------------------------
+
+    // Affichage des dernières valeurs
+
     svgPlot.append("text")
-        .attr("x", width + 8)
-        .attr("y", scaleY(maxVal[0].part_rea))
-        .text(maxVal[0].part_rea.toLocaleString('fr-FR'))
+        .attr("x", widthRatio + 8)
+        .attr("y", scaleY(maxVal[0].cum_dose2))
+        .text(Math.round(maxVal[0].cum_dose2 / 1000000, 2) + ' millions')
         .style("fill", "#D55E00");
 
     svgPlot.append("text")
-        .attr("x", width + 8)
-        .attr("y", scaleY(maxVal[0].part_hosp))
-        .text(maxVal[0].part_hosp.toLocaleString('fr-FR'))
+        .attr("x", widthRatio + 8)
+        .attr("y", scaleY(maxVal[0].cum_dose1))
+        .text(Math.round(maxVal[0].cum_dose1 / 1000000, 2) + ' millions')
         .style("fill", "#0072B2");
 
     //---------------------------------------------------------------------------------------
@@ -227,8 +251,8 @@ function showData(data) {
 
     // Objet contenant les informations à afficher dans la légende : text, couleur, opacité
     const legendeValues = [
-        { "label": "Hospitalisations", "col": "#0072B2", "op": 0.6 },
-        { "label": "Réanimations", "col": "#D55E00", "op": 1 }
+        { "label": "Au moins une dose", "col": "#0072B2", "op": 0.6 },
+        { "label": "Vaccination complète", "col": "#D55E00", "op": 1 }
     ];
 
     // Création d'un groupe g par élément de la légende (ici deux infos)
@@ -257,7 +281,3 @@ function showData(data) {
 
 
 }
-
-
-
-
